6edc2de9da86378b2bd7fe1b3d580ddd7be058e9
Chandu <Chandu@chandu.attlocal.net>
2016-01-09 10:53:32 -0800

    Finished First Weekly Assignment

--- skills.py
+++ skills.py
@@ -67,8 +67,9 @@ def long_words(word_list):
         []
 
     """
+    long_words = [word for word in word_list if len(word) > 4]
 
-    return ['the wrong thing']
+    return long_words
 
 
 def smallest_int(number_list):
@@ -91,7 +92,14 @@ def smallest_int(number_list):
 
     """
 
-    return 100
+    if number_list == []:
+        return None
+    else:
+        smallest_integer = number_list[0]
+        for num in number_list[1:]:
+            if num < smallest_integer:
+                smallest_integer = num
+        return smallest_integer
 
 
 def largest_int(number_list):
@@ -111,8 +119,14 @@ def largest_int(number_list):
         True
 
     """
-
-    return 0
+    if number_list == []:
+        return None
+    else:
+        largest_integer = number_list[0]
+        for num in number_list:
+            if num > largest_integer:
+                largest_integer = num        
+        return largest_integer
 
 
 def halvesies(number_list):
@@ -127,8 +141,8 @@ def halvesies(number_list):
         [0.5, 2.5]
 
     """
-
-    return []
+    half_number = [float(num) / 2 for num in number_list]
+    return half_number
 
 
 def word_lengths(word_list):
@@ -138,8 +152,8 @@ def word_lengths(word_list):
         [5, 3, 5, 4]
 
     """
-
-    return []
+    length_of_words = [len(word) for word in word_list]
+    return length_of_words
 
 
 def sum_numbers(number_list):
@@ -157,8 +171,10 @@ def sum_numbers(number_list):
         0
 
     """
-
-    return None
+    sum_of_numbers = 0
+    for num in number_list:
+        sum_of_numbers += num
+    return sum_of_numbers
 
 
 def mult_numbers(number_list):
@@ -179,8 +195,10 @@ def mult_numbers(number_list):
         1
 
     """
-
-    return None
+    product = 1
+    for num in number_list:
+        product *= num
+    return product
 
 
 def join_strings(word_list):
@@ -198,8 +216,10 @@ def join_strings(word_list):
         ''
 
     """
-
-    return "Not the right thing"
+    joined_string = ""
+    for word in word_list:
+        joined_string = joined_string + word
+    return joined_string
 
 
 def average(number_list):
@@ -211,8 +231,11 @@ def average(number_list):
     There is no defined answer if the list given is empty. It's fine if
     this raises an error when given an empty list.
     """
-
-    return 0
+    sum_of_numbers = sum_numbers(number_list)
+    num_of_numbers = 0
+    for num in number_list:
+        num_of_numbers += 1
+    return float(sum_of_numbers) / num_of_numbers
 
 
 def join_strings_with_comma(list_of_words):
@@ -228,8 +251,10 @@ def join_strings_with_comma(list_of_words):
         'Pretzel'
 
     """
-
-    return ""
+    joined_strings = list_of_words[0]
+    for word in list_of_words[1:]:
+        joined_strings = joined_strings + ", " + word
+    return joined_strings
 
 
 ##############################################################################
